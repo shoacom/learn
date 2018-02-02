585dd73ddcde294d252cda921fd83abd9db7bf67
xiojiang <xiongfei.jiang@nokia-sbell.com>
2018-02-02 16:24:45 +0800

    package

--- springboot-jetty-ws/webservice/src/main/java/com/shoa/springboot/webservice/security/SslContextFactoryProvider.java
+++ springboot-jetty-ws/webservice/src/main/java/com/shoa/springboot/webservice/security/SslContextFactoryProvider.java
@@ -1,17 +1,25 @@
 package com.shoa.springboot.webservice.security;
 
-import static com.shoa.springboot.webservice.Constants.*;
+import static com.shoa.springboot.webservice.Constants.KEYSTORE_PASS;
+import static com.shoa.springboot.webservice.Constants.KEYSTORE_PATH;
 
-import com.shoa.springboot.webservice.security.CipherSuites;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 /**
  * Created by xiojiang on 2017/8/22.
  */
 
 public class SslContextFactoryProvider extends SslContextFactory {
+  private static final Logger LOGGER = LoggerFactory.getLogger(SslContextFactoryProvider.class);
 
   public SslContextFactoryProvider() {
+    File file = new File(KEYSTORE_PATH);
+    LOGGER.info("Keystore path: " + file.getAbsolutePath());
+
     setKeyManagerPassword(KEYSTORE_PASS);
     setTrustStorePath(KEYSTORE_PATH);
     setTrustStorePassword(KEYSTORE_PASS);
